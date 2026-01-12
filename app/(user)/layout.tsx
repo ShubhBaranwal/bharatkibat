99cec525b6b45a9a0f4ae2464467a8740295bda1
Shubh Baranwal <baranwalshubh2@gmail.com>
2026-01-13 02:51:21 +0530

    fix issue of logo show in title

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -19,8 +19,25 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.bharatkibaat.com"),
   title: "Bharat Ki Baat – India's Latest News & Biographies",
+  description:
+    "Bharat Ki Baat brings verified Hindi news, politics updates, biographies, explainers and ground reports from India with trust and clarity.",
+  openGraph: {
+    type: "website",
+    siteName: "Bharat Ki Baat",
+    url: "https://www.bharatkibaat.com",
+    images: [
+      {
+        url: "https://www.bharatkibaat.com/logo.png",
+        width: 512,
+        height: 512,
+        alt: "Bharat Ki Baat Logo",
+      },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -28,13 +45,44 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="hi">
       <head>
         {/* ✅ AdSense Account Verification (SAFE & REQUIRED) */}
+        <meta property="og:site_name" content="Bharat Ki Baat" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_IN" />
+        <meta name="geo.region" content="IN" />
+        <meta name="geo.placename" content="India" />
+        <meta name="language" content="Hindi, English" />
+
+
+        <Script
+          id="organization-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Bharat Ki Baat",
+              "alternateName": "भारत की बात",
+              "url": "https://www.bharatkibaat.com",
+              "logo": "https://www.bharatkibaat.com/logo.png",
+              "sameAs": [
+                "https://www.facebook.com/profile.php?id=61584098969969",
+                "https://x.com/BatKi6140"
+              ]
+            })
+          }}
+        />
+
         <meta
           name="google-adsense-account"
           content="ca-pub-2792598202487179"
         />
+        <meta name="robots" content="index, follow, max-image-preview:large" />
+        <link rel="canonical" href="https://www.bharatkibaat.com" />
+
 
         {/* ✅ Google Analytics GA4 (USE next/script) */}
         <Script
